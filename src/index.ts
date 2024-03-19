91910497cfe37da527432a6934650e77f994b563
doguebart <douglasweelber.2004@outlook.com>
2024-03-19 14:17:08 -0300

    chore: project setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+app.use(cookieParser());
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
