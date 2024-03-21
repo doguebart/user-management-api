6a264ca2484c652947096c13d7acf64dcbf78978
doguebart <douglasweelber.2004@outlook.com>
2024-03-21 17:11:48 -0300

    feat: add protected route to get user information from cookie token

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,6 +2,11 @@ import express, { Response, Request } from "express";
 import { UserController } from "../controllers/user/UserController";
 import { verifyToken } from "../middlewares/verifyToken";
 
+interface AuthenticatedRequest extends Request {
+  userId?: string;
+  userRole?: string;
+}
+
 export const userRouter = express.Router();
 userRouter.use(express.json());
 const userController = new UserController();
@@ -14,6 +19,15 @@ userRouter.post("/logout", verifyToken, (req: Request, res: Response) => {
     .status(200)
     .json({ message: "Usuário desconectado com sucesso." });
 });
+userRouter.get(
+  "/protected",
+  verifyToken,
+  (req: AuthenticatedRequest, res: Response) => {
+    return res
+      .status(200)
+      .json({ user: { id: req.userId, role: req.userRole } });
+  }
+);
 userRouter.get("/", userController.getUsers.bind(userController));
 userRouter.get("/:id", userController.getUserById.bind(userController));
 userRouter.patch(
